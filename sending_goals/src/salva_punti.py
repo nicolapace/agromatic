7e9bb8af7e55ac7bac105875ebddea0e87e8fa3d
linoe97 <nicola.pace97@gmail.com>
2021-07-01 19:48:27 +0200

    salvataggio in json dei punti

--- sending_goals/src/salva_punti.py
+++ sending_goals/src/salva_punti.py
@@ -5,6 +5,7 @@ from geometry_msgs.msg import Pose
 from geometry_msgs.msg import PoseWithCovarianceStamped
 import time
 from os.path import expanduser
+import json
 
 
 class SavePoses(object):
@@ -12,31 +13,48 @@ class SavePoses(object):
         
         self._pose = Pose()
         # self.poses_list = []
+        self._poses_dict = {}
         self._pose_sub = rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped , self.sub_callback)
         self.write_to_file()
 
     def sub_callback(self, msg):
         
         self._pose = msg.pose.pose
+
     
     def write_to_file(self):
             
         i = 0
-        #aggiungere /agromatic
-        with open( str(expanduser("~"))+'/catkin_ws/src/sending_goals/pose/poses.txt', 'w') as file:
+       
+        with open('prova.json', 'w') as json_file:
+            
             while(1):
                 i+=1
                 value = input("Click \"s\" to save the waypoint.\nClick \"d\" to save file.\n")
                 print('You entered : '+ str(value) )
                 
                 if value=="s":
+                        self._poses_dict["posa" + str(i)] = {
+                            "posizione":{
+                                "x":self._pose.position.x,
+                                "y":self._pose.position.y,
+                                "z":self._pose.position.z
+                            }, 
+                            "orientamento":{
+                                "x":self._pose.orientation.x,
+                                "y":self._pose.orientation.y,
+                                "z":self._pose.orientation.z, 
+                                "w":self._pose.orientation.w
+                            }
+                        }
+                             
                         print("Posa salvata!\n"+str(self._pose))
-                        file.write("posa n."+str(i) + ':\n----------\n' + str(self._pose) + '\n===========\n')
 
                 if value=="d":
+                    json.dump(self._poses_dict,json_file)
                     break        
                     
-        rospy.loginfo("Written all Poses to poses.txt file")
+        rospy.loginfo("Written all Poses to poses.json file")
         
 
 
