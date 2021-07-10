43107bbbe6c51e4accac20ad5f3cb122d4f77da3
linoe97 <nicola.pace97@gmail.com>
2021-07-10 19:00:04 +0200

    gestione raccolta pomodori

--- sending_goals/src/sg_spawn_tomato.py
+++ sending_goals/src/sg_spawn_tomato.py
@@ -13,7 +13,7 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseActionResul
 from geometry_msgs.msg import Pose, PoseStamped, PoseArray, Quaternion, Point
 from tf.transformations import quaternion_from_euler
 from actionlib_msgs.msg import GoalStatusArray, GoalStatus
-from gazebo_msgs.srv import SpawnModel
+from gazebo_msgs.srv import DeleteModel, SpawnModel
 from gazebo_msgs.srv import GetModelState
 #json
 import json
@@ -30,9 +30,12 @@ class Sending_goal():
         rospy.wait_for_service("/gazebo/spawn_sdf_model")
         self._status_sub = rospy.Subscriber('/move_base/status', GoalStatusArray , self.sub_callback)
         self.pub = rospy.Publisher('move_base_simple/goal', PoseStamped, queue_size=10)
-        self.t = random.randint(250,250)
-        print("Tempo timer: " + str(self.t))
-        self.t_ini = time.time() 
+        self.maxWeight=50*0.09
+
+        self.spawn_model_client = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
+        self.delete_model = rospy.ServiceProxy("gazebo/delete_model", DeleteModel)
+
+        self.nFruits = 0
         self.talker()
 
     def setPose(self,pose):
@@ -50,22 +53,41 @@ class Sending_goal():
         
         self.pub.publish(p)
 
-    def contr_timer(self,pose):
-        print("tempo passato "+ str(time.time()-self.t_ini))
-        if time.time()-self.t_ini>self.t:
+    def svuota_cassetta(self,i):
+        #svuoto la cassetta
+            for x in range(0, self.nFruits):
+                self.delete_model("Fruit"+str(x))
+
+                posa = Pose()
+                posa.position.x = -7+random.randint(0,600)/1000-0.3
+                posa.position.y = -9+random.randint(0,600)/1000-0.3
+                posa.position.z = 2.5
+                posa.orientation = Quaternion(*quaternion_from_euler(0.0, 0.0, 0.0))
+                self.spawn_model_client('Fruit_pick_up'+str(x)+str(i),
+                                        open(str(expanduser("~"))+"/.gazebo/models/tomato_fruit/model.sdf",'r').read(),
+                                        "/foo",
+                                        posa ,
+                                        "map")
+            self.nFruits = 0
+
+    def contr_timer(self,pose,i):
+        print("Peso attuale: "+ str(self.nFruits*0.09))
+        if self.nFruits*0.09>=self.maxWeight:
             p = PoseStamped()
-            p.pose.position.x = -7  # posizione del check-point
-            p.pose.position.y = -6
+            p.pose.position.x = -6.9 # posizione del check-point
+            p.pose.position.y = -6.1
             p.pose.position.z = 0
             p.header.frame_id="map"
-            q = quaternion_from_euler(0.0, 0.0, 0.0)
+            q = quaternion_from_euler(0.0, 0.0, 0.022140)
             p.pose.orientation = Quaternion(*q)
             self.pub.publish(p)
             print("RITORNO ALLA BASE")
             while self._status.status != 3:
                 rospy.sleep(1.)
                 print("Ritorno...")
-          
+
+            self.svuota_cassetta(i)
+
             #recupero il goal che avevo sospeso
             self.setPose(pose)
             print("x: " + str(pose['posizione']['x']) + ", y: "+ str(pose['posizione']['y']))
@@ -73,34 +95,46 @@ class Sending_goal():
             while self._status.status != 3:
                         print(str(self._status.text))
                         rospy.sleep(1.)
-            self.t = random.randint(250,250)
-            self.t_ini = time.time()
+                        self.spawn_fruits(1)
+            
 
 
     def finalpose(self):
         p = PoseStamped()
-        p.pose.position.x = -7 # posizione del check-point
-        p.pose.position.y = -6
+        p.pose.position.x = -6.9 # posizione del check-point
+        p.pose.position.y = -6.1
         p.pose.position.z = 0
         p.header.frame_id="map"
-        q = quaternion_from_euler(0.0, 0.0, 0.0)
+        q = quaternion_from_euler(0.0, 0.0, 0.022140)
         p.pose.orientation = Quaternion(*q)
         self.pub.publish(p)
         print("FINITO: RITORNO ALLA BASE")
+        rospy.sleep(1.)
         while self._status.status != 3:
             rospy.sleep(1.)
             print("Ritorno...")
+        self.svuota_cassetta(10e5)
+
+    def spawn_fruits(self,n):
+        n_fruits = int(random.randint(int(n),2*int(n))/2)
+        for i in range(self.nFruits,self.nFruits+n_fruits):
+            posa = Pose()
+            posa.position.x = -0.07+random.randint(0,8)/100-0.04
+            posa.position.y = 0+random.randint(0,8)/100-0.04
+            posa.position.z = 0.8
+            posa.orientation = Quaternion(*quaternion_from_euler(0.0, 0.0, 0.0))
+            self.spawn_model_client('Fruit'+str(i),
+                                    open(str(expanduser("~"))+"/.gazebo/models/tomato_fruit/model.sdf",'r').read(),
+                                    "/foo",
+                                    posa ,
+                                    "base_link")
+        self.nFruits+=n_fruits
+                   
 
     #callback dello status 
     def sub_callback(self, msg):
         if(len(msg.status_list)>0):
             self._status  = msg.status_list[len(msg.status_list)-1]
-
-        # print("goal list size: " + str(len(self._status)))
-        # for stat in self._status:
-        #     print("status_id: " + str(stat.status))
-        #     print("status_text: " + stat.text)
-        # print("il valore è: "+str(self._status)) 
 
     def talker(self):
         file = open(str(expanduser("~"))+'/catkin_ws/src/sending_goals/pose/poses.json',)
@@ -112,15 +146,21 @@ class Sending_goal():
                 print("x: " + str(pose['posizione']['x']) + ", y: "+ str(pose['posizione']['y']))
                 self.setPose(pose)
                 print(self._status.text)
+                
             elif i==1:
                 print("Goal numero: "+str(i))
                 print("x: " + str(pose['posizione']['x']) + ", y: "+ str(pose['posizione']['y']))
                 self.setPose(pose)
+                rospy.sleep(1.)
+                self.setPose(pose)
                 print(self._status.text)
                 while self._status.status != 3:
-                    self.contr_timer(pose) 
+                    self.contr_timer(pose,i) 
                     print(self._status.text)
                     rospy.sleep(1.)
+                    self.spawn_fruits(1)
+                self.spawn_fruits(6)
+               
             else:
                 print("Goal numero: "+str(i))
                 print(self._status.text)
@@ -129,25 +169,12 @@ class Sending_goal():
                     print("x: " + str(pose['posizione']['x']) + ", y: "+ str(pose['posizione']['y']))
                     rospy.sleep(1.)
                     while self._status.status != 3:
-                        self.contr_timer(pose) # Quando il timer scade viene inviata la posizione del 
+                        self.contr_timer(pose,i) # Quando il timer scade viene inviata la posizione del 
                         # check-point e in seguito rinviato all'ultima posa raggiunta
                         print(self._status.text)
                         rospy.sleep(1.)
-                    spawn_model_client = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
-                    # self.object_coordinates = self.model_coordinates("husky", "")
-                    # y = self.object_coordinates.pose.position.y
-                    # x = self.object_coordinates.pose.position.x
-                    posa = Pose()
-                    posa.position.x = -0.1
-                    posa.position.y = 0
-                    posa.position.z = 1
-                    posa.orientation = Quaternion(*quaternion_from_euler(0.0, 0.0, 0.0))
-                    spawn_model_client('Fruit'+str(i), open(str(expanduser("~"))+"/.gazebo/models/tomato_fruit/model.sdf",'r').read(), "/foo", posa ,"base_link")
-                   
-                # else:
-                #     while self._status.status != 3:
-                #         print(str(self._status.text))
-                #         rospy.sleep(1.)
+                        self.spawn_fruits(1)
+                    self.spawn_fruits(6)
             i+=1 
             self._rate.sleep()
 
--- waypoint_nav/husky_customization/husky_custom_description/scripts/remove_plate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e #exit on first error
-
-export HUSKY_TOP_PLATE_ENABLED="false"
