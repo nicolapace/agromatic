bdd9f351af4b6c580b5f442584a0b1ab193f9eeb
linoe97 <nicola.pace97@gmail.com>
2021-07-23 14:33:19 +0200

    added water tank and corrected dynamic map

--- husky_customization/husky_custom_description/env-hooks/60.husky_custom_description.sh
+++ husky_customization/husky_custom_description/env-hooks/60.husky_custom_description.sh
@@ -1,1 +1,2 @@
 export HUSKY_DESCRIPTION=$(catkin_find --first-only --without-underlays husky_custom_description urdf/custom_description.urdf.xacro 2>/dev/null)
+export HUSKY_DESCRIPTION_WATER_TANK=$(catkin_find --first-only --without-underlays husky_custom_description urdf/custom_water_tank.urdf.xacro 2>/dev/null)
